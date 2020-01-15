9921595c0e9a5267127e1b03bfb946392d2fa5bf
Lucas Marçal Coutinho <coutinho0604@gmail.com>
2020-01-15 01:33:06 -0300

    A um passo da vitória

--- js/script-padrao.js
+++ js/script-padrao.js
@@ -1,12 +1,20 @@
 //Calculadora
 
 
+
 var visor;
 var preview;
 var resposta = 'CATCHAU';
-var operacao_com_parenteses = 0;
 
-//Gerador de Cor
+
+
+//Essa variável serve para saber se a última operação feita foi a de Raiz ou 1/X.
+var operacao_com_parenteses = 0;
+//
+
+
+
+//Gerador de Cores.
 function getRandomColor() {
     var letters = "0123456789ABCDEF";
     var color = "#";
@@ -16,7 +24,10 @@ function getRandomColor() {
     return color;
 }
 //
-//Teste
+
+
+
+//Muda as cores do site de acordo com a cor gerada na função anterior.
 var cor = getRandomColor();
 function mudaCor() {
     var fundo = window.document.querySelector("body");
@@ -33,138 +44,241 @@ function mudaCor() {
 }
 //
 
-var contador = 0;
-var contNegado= 0;
-function operacao(op) {
-    //console.log(v)
 
+
+//OPERAÇÕES DA CALCULADORA
+
+
+
+//Essa funçãoclimita número com muitas casas decimais à apenas 5 casas decimais.
+function limita_casas_decimais() {
+    var decimal = Number(resposta);
+    var casas_decimais = 0;
+    var truncado = Math.trunc(resposta);
+    console.log("truncado: " + truncado);
+    console.log(resposta);
+    console.log("casas decimais: " + decimal);
+    if (decimal != truncado) {
+        while (decimal != truncado) {
+            truncado = Math.trunc(decimal * 10);
+            decimal = decimal * 10;
+            console.log("truncado: " + truncado);
+            console.log("casas decimais: " + decimal);
+            casas_decimais++;
+            if (casas_decimais > 5) {
+                break;
+            }
+        }
+        console.log("casas decimais - tam: " + casas_decimais);
+        if (casas_decimais > 5) {
+            resposta = resposta.toFixed(5)
+            console.log("res 5 casas: " + resposta)
+        }
+    }
+}
+//
+
+
+
+//
+function basic_ops(num) {
+    last_num = visor.textContent;
+    console.log("Ultimo numero digitado= " + last_num);
+
+    if (preview.textContent[preview.textContent.length - 2] == '*') {
+        resposta = Number(resposta) * num;
+    } else if (preview.textContent[preview.textContent.length - 2] == '/') {
+        resposta = Number(resposta) / num;
+    } else if (preview.textContent[preview.textContent.length - 2] == '+') {
+        resposta = Number(resposta) + num;
+    } else if (preview.textContent[preview.textContent.length - 2] == '-') {
+        resposta = Number(resposta) - num;
+    } else if (preview.textContent[preview.textContent.length - 2] == '^') {
+        resposta = Math.pow(Number(resposta), num);
+    }
+}
+//
+
+
+
+var contador = 0;
+var contNegado = 0;
+//guardará o número digitado quando o igual for apertado.
+var last_num;
+
+
+
+function operacao(op) {
+
+
+
+    //Esse contador serve para...
     if (op != '=') {
         contador++;
     }
+    //
 
+
+
+    //Esses são os "visores" da calculadora.
     preview = window.document.querySelector("p#preview");
     visor = window.document.querySelector("p#resposta");
+    //
+
+
+
+    //
+    if (op == '%') {
+        /*
+        if (preview.innerHTML == ``) {
+            visor.textContent = 0;
+        }
+        */
+       return false;
+    }
+    //
+
+
+
+    //Esta operação apaga o conteúdo de todos os "visores" e as operações armazenadas.
     if (op == 'C') {
         preview.innerHTML = ``;
         visor.textContent = '';
         resposta = 'CATCHAU';
-
         return false;
     }
+    //
 
+
+
+    //Essa operação apaga o conteúdo apenas do visor maior.
     if (op == 'CE') {
         visor.textContent = '';
-
         return false;
     }
-    //console.log(visor.textContent.length)
+    //
 
+
+
+    //Essa operação apaga o conteúdo apenas o último número digitado no "visor principal".
     if (op == 'DEL' && visor.textContent != '0') {
-        var delecao = Number(visor.textContent) - Number(visor.textContent[visor.textContent.length - 1]);
-        delecao = delecao / 10;
-        if (delecao == 0) {
-            visor.textContent = '0'
-        } else {
-            visor.textContent = delecao;
-        }
+        var delecao = visor.textContent;
+        delecao = delecao.substring(0, (delecao.length - 1));
+        visor.textContent = delecao;
         return false;
     }
+    //
 
+
+
+    //Essa operação inverte o número digitado no "visor principal" (multiplica por -1).
     if (op == '+/-' && visor.textContent != '') {
         var negar = Number(visor.textContent);
         negar = negar * (-1);
-        operacao_com_parenteses++;
-        
+        //operacao_com_parenteses++;
         if (preview.innerHTML != '') {
-            if (preview.textContent[preview.textContent.length - 2] == '*') {
-                resposta = resposta * negar;
-            } else if (preview.textContent[preview.textContent.length - 2] == '/') {
-                resposta = resposta / negar;
-            } else if (preview.textContent[preview.textContent.length - 2] == '+') {
-                resposta = resposta + negar;
-            } else if (preview.textContent[preview.textContent.length - 2] == '-') {
-                resposta = resposta - negar;
-            } else if (preview.textContent[preview.textContent.length - 2] == '%') {
-                resposta = resposta % negar;
-            } else if (preview.textContent[preview.textContent.length - 2] == '^') {
-                resposta = Math.pow(resposta, negar);
-            }
+
             operacao_com_parenteses = 0;
-        }else{
+
+        } else {
             resposta = negar;
         }
         contador = 0;
         contNegado = 1;
+
+        limita_casas_decimais();
+
         /*
         preview.innerHTML += `${visor.textContent} * (-1) `;
         */
+
         visor.textContent = negar;
 
+        last_num = visor.textContent;
+        console.log("Ultimo numero digitado= " + last_num);
         return false;
     }
+    //
 
+
+
+    //
     if (op == '1/X' && visor.textContent != '') {
+        
         var um_sobre_x = Number(visor.textContent);
+        console.log("1/X -> " + um_sobre_x);
         um_sobre_x = 1 / um_sobre_x;
-        operacao_com_parenteses++;
-        if (preview.innerHTML != '') {
-            if (preview.textContent[preview.textContent.length - 2] == '*') {
-                resposta = resposta * um_sobre_x;
-            } else if (preview.textContent[preview.textContent.length - 2] == '/') {
-                resposta = resposta / um_sobre_x;
-            } else if (preview.textContent[preview.textContent.length - 2] == '+') {
-                resposta = resposta + um_sobre_x;
-            } else if (preview.textContent[preview.textContent.length - 2] == '-') {
-                resposta = resposta - um_sobre_x;
-            } else if (preview.textContent[preview.textContent.length - 2] == '%') {
-                resposta = resposta % um_sobre_x;
-            } else if (preview.textContent[preview.textContent.length - 2] == '^') {
-                resposta = Math.pow(resposta, um_sobre_x);
-            }
-            operacao_com_parenteses = 0;
-        }else{
+        console.log("2 - 1/X -> " + um_sobre_x);
+        if (preview.innerHTML != '' && operacao_com_parenteses == 0) {
+
+            basic_ops(um_sobre_x);
+
+        } else {
             resposta = um_sobre_x;
         }
-        preview.innerHTML += `1/(${visor.textContent}) `;
+
+        limita_casas_decimais();
+
+        if(operacao_com_parenteses > 0){
+            preview.innerHTML = `1/(${preview.innerHTML}) `;
+        }else{
+            preview.innerHTML += `1/(${visor.textContent}) `;
+        }
+        
+        operacao_com_parenteses++;
         visor.textContent = resposta;
+
+        last_num = visor.textContent;
+        console.log("Ultimo numero digitado= " + last_num);
         return false;
     }
+    //
 
+
+
+    //
     if (op == 'sqrt' && visor.textContent != '') {
         var raizQ = Number(visor.textContent);
         raizQ = Math.sqrt(raizQ);
-        operacao_com_parenteses++;
 
-        if (preview.innerHTML != '') {
-            if (preview.textContent[preview.textContent.length - 2] == '*') {
-                resposta = resposta * raizQ;
-            } else if (preview.textContent[preview.textContent.length - 2] == '/') {
-                resposta = resposta / raizQ;
-            } else if (preview.textContent[preview.textContent.length - 2] == '+') {
-                resposta = resposta + raizQ;
-            } else if (preview.textContent[preview.textContent.length - 2] == '-') {
-                resposta = resposta - raizQ;
-            } else if (preview.textContent[preview.textContent.length - 2] == '%') {
-                resposta = resposta % raizQ;
-            } else if (preview.textContent[preview.textContent.length - 2] == '^') {
-                resposta = Math.pow(resposta, raizQ);
-            }
-            operacao_com_parenteses = 0;
-        }else{
+        if (preview.innerHTML != '' && operacao_com_parenteses == 0) {
+
+            basic_ops(raizQ);
+
+        } else {
             resposta = raizQ;
         }
 
-        preview.innerHTML = `sqrt(${visor.textContent}) `;
+        limita_casas_decimais();
+
+        if(operacao_com_parenteses > 0){
+            preview.innerHTML = `sqrt(${preview.innerHTML}) `;
+        }else{
+            preview.innerHTML += `sqrt(${visor.textContent}) `;
+        }
+
+        operacao_com_parenteses++;
         visor.textContent = resposta;
+
+        last_num = visor.textContent;
+        console.log("Ultimo numero digitado= " + last_num);
         return false;
     }
+    //
 
+
+
+    //
     if (op == ',' && visor.textContent != '') {
         visor.textContent += '.';
         contador = 0;
         return false;
     }
+    //
 
+
+
+    //
     if (visor.textContent == undefined || visor.textContent == '' || visor.textContent == null || visor.textContent == 0) {
         return false;
     } else {
@@ -180,92 +294,91 @@ function operacao(op) {
             return false;
         }
     }
+    //
 
-    /* A PROCENTAGEM ESTÁ ERRADA */
+
 
     // Para Raiz e 1/X
-    if (preview.textContent[preview.textContent.length - 2] == ')') {
+    if (preview.textContent[preview.textContent.length - 2] == ')' && op != '=') {
         operacao_com_parenteses = 0;
         preview.innerHTML += `${op} `;
         return false;
     }
     //
 
+
+
+    //
     if (resposta != 'CATCHAU') {
         console.log("OP")
-        if (preview.textContent[preview.textContent.length - 2] == '*') {
-            resposta = resposta * Number(visor.textContent);
-        } else if (preview.textContent[preview.textContent.length - 2] == '/') {
-            resposta = resposta / Number(visor.textContent);
-        } else if (preview.textContent[preview.textContent.length - 2] == '+') {
-            resposta = resposta + Number(visor.textContent);
-        } else if (preview.textContent[preview.textContent.length - 2] == '-') {
-            resposta = resposta - Number(visor.textContent);
-        } else if (preview.textContent[preview.textContent.length - 2] == '%') {
-            resposta = resposta % Number(visor.textContent);
-        } else if (preview.textContent[preview.textContent.length - 2] == '^') {
-            resposta = Math.pow(resposta, Number(visor.textContent));
-        }
+
+        basic_ops(Number(visor.textContent));
+
+        limita_casas_decimais();
 
     }
+    //
+
+
+
+    //
     console.log(preview.textContent[preview.textContent.length - 2])
     console.log(resposta)
+    //
+
+
+
+    //
     if (op == '=' && preview.textContent != '' && visor.textContent != '') {
 
-        var decimal = resposta;
-        var casas_decimais = 0;
-        var truncado = Math.trunc(resposta);
-        console.log("truncado: " + truncado);
-        console.log(resposta);
-        console.log("casas decimais: " + decimal);
-        if (decimal != truncado) {
-            while (decimal != truncado) {
-                truncado = Math.trunc(decimal * 10);
-                decimal = decimal * 10;
-                console.log("truncado: " + truncado);
-                console.log("casas decimais: " + decimal);
-                casas_decimais++;
-                if (casas_decimais > 5) {
-                    break;
-                }
-            }
-            console.log("casas decimais - tam: " + casas_decimais);
+        last_num = visor.textContent;
+        console.log("Ultimo numero digitado= " + last_num);
 
-            if (casas_decimais > 5) {
-                resposta = resposta.toFixed(5)
-                console.log("res 5 casas: " + resposta)
-            }
-        } else {
-            resposta = resposta.toFixed(0)
-        }
+        limita_casas_decimais();
+
         preview.innerHTML = ``;
         visor.textContent = resposta;
         resposta = 'CATCHAU';
         return false;
     }
-    if (preview.textContent[preview.textContent.length - 2] == ')') {
+    //
+
+
+
+    //
+    if (preview.textContent[preview.textContent.length - 2] == ')' && op != '=') {
         preview.innerHTML += `${op} `;
     } else {
         preview.innerHTML += `${visor.textContent} ${op} `;
     }
+    //
+
+
+
+    //
     visor.textContent = resposta;
+    //
+
+
+
 }
+// Fim função operação
 
 var tamanho_num;
 
 function numero(n) {
     visor = window.document.querySelector("p#resposta");
     tamanho_num = visor.textContent.length;
-    
-    if (operacao_com_parenteses > 0  && contNegado == 0) {
+
+    if (operacao_com_parenteses > 0 && contNegado == 0) {
         preview.innerHTML = ``;
         visor.textContent = '';
         operacao_com_parenteses = 0;
         contNegado = 0;
     }
-    
+
     //console.log()
-    if (tamanho_num <= 5) {
+    if (tamanho_num <= 7) {
         if (visor.textContent == '0' || contador > 0) {
             visor.innerHTML = n;
             contador = 0;
@@ -277,35 +390,31 @@ function numero(n) {
     }
 
 }
-//console.log(tamanho_num)
+
 function colorirNum(n) {
-    //console.log("HI")
-    //console.log(tamanho_num)
     var bloco = window.document.querySelector("input#" + n);
     bloco.style.background = '#ddddcc';
 }
 
+
+
 function descolorirNum(n) {
-    //console.log("HI")
     var bloco = window.document.querySelector("input#" + n);
     bloco.style.background = '#ffffff';
 }
 
+
+
 function colorirOp(op) {
-    //console.log("HI")
     var bloco = window.document.querySelector("input#" + op);
     bloco.style.background = cor;
     bloco.style.color = '#ffffff'
 }
 
+
+
 function descolorirOp(op) {
-    //console.log("HI")
     var bloco = window.document.querySelector("input#" + op);
     bloco.style.background = '#ffffff';
     bloco.style.color = cor
 }
-
-
-
-//Histórico
-
